c9a87396fe47e20c3be76289e8634651c04f2531
iiiaamaah <m.s.shamaev@gmail.com>
2022-10-06 00:07:03 +0300

    сделаны домашние работы из уроков 6, 7, 8

--- Lesson_002/08_garden.py
+++ Lesson_002/08_garden.py
@@ -10,18 +10,27 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # создайте множество цветов, произрастающих в саду и на лугу
 # garden_set =
 # meadow_set =
-# TODO здесь ваш код
+garden_set = set(garden)
+meadow_set = set(meadow)
+flowers_set = garden_set.union(meadow_set)
 
 # выведите на консоль все виды цветов
-# TODO здесь ваш код
+
+print(flowers_set)
 
 # выведите на консоль те, которые растут и там и там
-# TODO здесь ваш код
+
+flowers_both = garden_set.intersection(meadow_set)
+print(flowers_both)
 
 # выведите на консоль те, которые растут в саду, но не растут на лугу
-# TODO здесь ваш код
+
+flowers_garden = garden_set.difference(meadow_set)
+print(flowers_garden)
 
 # выведите на консоль те, которые растут на лугу, но не растут в саду
-# TODO здесь ваш код
+
+flowers_meadow = meadow_set.difference(garden_set)
+print(flowers_meadow)
 
 
